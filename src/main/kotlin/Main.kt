a54e4e812460f33eb1f6e35f91c4f03322c3a1ae
Diego Cano Sibón <dcsibon@gmail.com>
2024-01-26 21:26:40 +0100

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,26 @@
-/*
-* Corregir los errores para que el juego funcione.
-* Documentar el código.
-* Realizar las siguientes mejoras o modificaciones en el código:
-    1. Salir del juego en cualquier momento si pulso la tecla ENTER sin introducir nada.
-    2. Incluir el texto: Turno del jugador 1 ó 2 según sea el caso.
-    3. Incluye una función para limpiar la consola... cómo no es posible en Kotlin, te propongo que tu método reciba un número y hagas un bucle dónde se imprima por consola un salto de línea tantas veces cómo el número que recibe.
-    4. Modifica el programa para controlar con try-catch errores a la hora de introducir la posición en el tablero, muestre un mensaje de error y vuelva a pedir la posición (fila o columna).
-    5. El juego debe mostrar el tablero cómo el siguiente:
-    -------------
-    | X |   |   |
-    -------------
-    |   | O |   |
-    -------------
-    |   |   | X |
-    -------------
-    6. Desarrolla la función que está solo declarada "checkWinner", para comprobar si algún jugador ha ganado.
-*/
-
-fun clearConsole_dcs(num: Int) {
-    (1..num).forEach{ println() }
-}
 
 fun clearConsole(num: Int) {
-    //TODO: Limpiar la consola con un número de saltos de línea pasados por parámetro
+    (1..num).forEach{ _ -> println() }
 }
 
 fun printRow(row: IntArray) {
     // Mostrar una fila del tablero
-    //println(" " + row.joinToString(" | ") { if (it == 0) " " else if (it == 1) "X" else "O" })
     println("| " + row.joinToString(" | ") { if (it == 0) " " else if (it == 1) "X" else "O" } + " |")
     println("-------------")
 }
 
-fun printBoard_dcs(board: Array<IntArray>) {
+fun printBoard(board: Array<IntArray>) {
     // Mostrar en consola el tablero con las fichas
-    clearConsole()
+    clearConsole(20)
     println("-------------")
     board.forEach { printRow(it) }
 }
-
-fun printBoard() {
-    // Mostrar en consola el tablero con las fichas
-    clearConsole()
-    println("-------------")
-    board.forEach { printRow(it) }
-}
-
-fun createBoard_dcs(size: Int = 3) = Array(size) { IntArray(size) }
 
 fun createBoard(size: Int = 3) = Array(size) { IntArray(size) }
 
-fun checkWinner_dcs(board: Array<IntArray>): Int {
+fun checkWinner(board: Array<IntArray>): Int {
     // Comprobar si algún jugador ha ganado
-    for (i in 0 until 3) {
+    for (i in 0..<3) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0) {
             return board[i][0]
         }
@@ -69,36 +37,12 @@ fun checkWinner_dcs(board: Array<IntArray>): Int {
     return 0
 }
 
-fun checkWinner(board: Array<IntArray>): Int {
-    //TODO: Desarrolla esta función para comprobar si algún jugador ha ganado
-    // Comprobar si algún jugador ha ganado
-    return 0
-}
-
 fun isBoardFull(board: Array<IntArray>): Boolean {
     // Verificar si el tablero está lleno
     return board.all { row -> row.all { it != 0 } }
 }
 
-fun placePiece(board: Array<IntArray>, player: Int) {
-    val salir = false
-    while (salir) {
-        print("Elige la fila (1, 2, 3): ")
-        val row = readln().toInt().minus(1)
-
-        print("Elige la columna (1, 2, 3): ")
-        val col = readln().toInt().minus(1)
-
-        if (row in 1 until 4 && col in 1 until 4 && board[row][col] == " ") {
-            board[row][col] = player
-            salir = true
-        } else {
-            println("**Error** Movimiento inválido. Inténtalo de nuevo.")
-        }
-    }
-}
-
-fun getInput_dcs(msj: String) : Int {
+fun getInput(msj: String) : Int {
     print(msj)
     val input = readln()
     var value = 0
@@ -115,17 +59,17 @@ fun getInput_dcs(msj: String) : Int {
                 println("**Error** Número inválido. Inténtalo de nuevo.")
             }
         }
-    } while (value in 0..3)
+    } while (value !in 0..3)
 
     return value
 }
 
-fun placePiece_dcs(board: Array<IntArray>, player: Int) {
+fun placePiece(board: Array<IntArray>, player: Int) {
     var salir = false
     while (!salir) {
-        val row = getInput_dcs("Elige la fila (1, 2, 3): ")
+        val row = getInput("Elige la fila (1, 2, 3): ")
 
-        val col = getInput_dcs("Elige la columna (1, 2, 3): ")
+        val col = getInput("Elige la columna (1, 2, 3): ")
 
         if (board[row][col] == 0) {
             board[row][col] = player
@@ -136,12 +80,7 @@ fun placePiece_dcs(board: Array<IntArray>, player: Int) {
     }
 }
 
-fun switchPlayer(player: Int): String {
-    // Cambiar de jugador
-    return if (player == 1) "Jugador 1" else "Jugador2"
-}
-
-fun switchPlayer_dcs(player: Int): Int {
+fun switchPlayer(player: Int): Int {
     // Cambiar de jugador
     return if (player == 1) 2 else 1
 }
@@ -157,6 +96,7 @@ fun main() {
         placePiece(board, currentPlayer)
 
         winner = checkWinner(board)
+
         if (winner != 0) {
             printBoard(board)
             println("¡El jugador $winner ha ganado!")
